44eb4868408e93d05e30acf6b0adf927e16c1924
Lauro Moura <lauromoura@gmail.com>
2018-09-21 16:27:53 -0300

    Add keyboard interaction.

--- 2018/webassembly/code/emscripten/04-c-loop/hello-sdl.cpp
+++ 2018/webassembly/code/emscripten/04-c-loop/hello-sdl.cpp
@@ -11,17 +11,42 @@ struct context {
     SDL_Renderer *renderer;
     SDL_Texture *image;
     SDL_Rect rect;
+    bool running;
+    int angle;
 };
 
 extern "C" {
+
+static inline bool key_up(SDL_Event e, SDL_Keycode key)
+{
+    return e.type == SDL_KEYUP && e.key.keysym.sym == key;
+}
 
 void loop_fn(void *data)
 {
     context *c = (context*)data;
     static int i = 0;
 
+    SDL_Event e;
+    if (SDL_PollEvent(&e)) {
+        if (key_up(e, SDLK_ESCAPE)) {
+            printf("Exiting loop...\n");
+            c->running = false;
+#ifdef __EMSCRIPTEN__
+            emscripten_cancel_main_loop();
+#endif
+            return;
+        }
+
+        if (key_up(e, SDLK_LEFT))
+            c->angle -= 15;
+        else if (key_up(e, SDLK_RIGHT))
+            c->angle += 15;
+    }
+
+
     SDL_RenderClear(c->renderer);
-    SDL_RenderCopy(c->renderer, c->image, NULL, &c->rect);
+    SDL_RenderCopyEx(c->renderer, c->image, NULL, &c->rect, c->angle, 0, SDL_FLIP_NONE);
     SDL_RenderPresent(c->renderer);
 
     i++;
@@ -31,12 +56,16 @@ void loop_fn(void *data)
 int main() {
     printf("Starting SDL demo!\n");
 
-    const int SIZE = 480;
+    const int SIZE = 470;
+    const char* filename = "native_floripa.png";
+    int w, h;
 
     SDL_Init(SDL_INIT_VIDEO);
     IMG_Init(IMG_INIT_PNG);
 
     context ctx;
+    ctx.running = true;
+    ctx.angle = 0;
 
 
     printf("Opening screen\n");
@@ -51,14 +80,14 @@ int main() {
         printf("Failed to create renderer: %s\n", SDL_GetError());
         return 1;
     }
-    const char* filename = "native_floripa.png";
+
     printf("Opening image\n");
     ctx.image = IMG_LoadTexture(ctx.renderer, filename);
     if (!ctx.image) {
         printf("Failed to get image %s: %s\n", filename, IMG_GetError());
         return 1;
     }
-    int w, h;
+
     SDL_QueryTexture(ctx.image, NULL, NULL, &w, &h);
 
     ctx.rect.x = 0;
@@ -75,6 +104,16 @@ int main() {
 #else
 
     // FIXME add regular main loop
+    while (ctx.running) {
+        loop_fn(&ctx);
+
+        SDL_Delay(1000/60);
+    }
+
+    SDL_DestroyTexture(ctx.image);
+    SDL_DestroyRenderer(ctx.renderer);
+    SDL_DestroyWindow(ctx.screen);
+    SDL_Quit();
 
 #endif
     return 0;
