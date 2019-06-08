c54a8c670b9bfffed5b9a9cbd537c5aa9f470be6
Lauro Moura <lauromoura@gmail.com>
2019-06-07 23:04:24 -0300

    Updates

---
+++ precache-manifest.807c5309e77549c858383db44356f382.js
@@ -0,0 +1,39 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "0c62e279077436571086dac3440e88b1.png"
+  },
+  {
+    "url": "24e132cbbeaf08e4cecc878895e3f01b.png"
+  },
+  {
+    "url": "de1a5562b8a8884356a8eec9e898c6d0.png"
+  },
+  {
+    "revision": "ce0574bad2b90c47e8194165627a61e2",
+    "url": "index.html"
+  },
+  {
+    "url": "main.1.307630f07fa759f437d3.bundle.js"
+  },
+  {
+    "url": "presenter.view.2.dd727f9d2edd34a3f36f.bundle.js"
+  },
+  {
+    "url": "runtime.0bc5594c550828050599.bundle.js"
+  },
+  {
+    "url": "target-blank.4.be006ab5e769ce0407c3.bundle.js"
+  },
+  {
+    "url": "vendor.5.6bd2b7899ae9e6ce1a28.bundle.js"
+  },
+  {
+    "url": "vendor.5.6bd2b7899ae9e6ce1a28.css"
+  },
+  {
+    "url": "vendors~Sidebar.0.27d9794a9d38e0dba37b.bundle.js"
+  },
+  {
+    "url": "vendors~Sidebar.0.27d9794a9d38e0dba37b.css"
+  }
+]);
